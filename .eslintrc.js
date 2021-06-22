d145566da9e1455aa4f93fdb70df420fe2a858cf
Susisu <susisu2413@yahoo.co.jp>
2021-06-22 15:30:16 +0900

    build: setup build, lint, test, etc.

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+"use strict";
+
+module.exports = {
+  plugins: ["jest", "jest-formatting"],
+  overrides: [
+    // source files
+    {
+      files: ["src/*.{ts,tsx}"],
+      extends: [
+        "@susisu/eslint-config/preset/ts-types",
+        "plugin:eslint-comments/recommended",
+        "prettier",
+      ],
+      parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      env: {
+        es6: true,
+        browser: true,
+      },
+      rules: {
+        "eslint-comments/no-unused-disable": "error",
+      },
+    },
+    // test files
+    {
+      files: ["src/*.{test,spec}.{ts,tsx}", "src/**/__tests__/**/*.{ts,tsx}"],
+      extends: ["plugin:jest/recommended", "plugin:jest-formatting/recommended"],
+      env: {
+        "jest/globals": true,
+      },
+    },
+    // script files
+    {
+      files: ["*.js"],
+      extends: [
+        "@susisu/eslint-config/preset/es",
+        "plugin:eslint-comments/recommended",
+        "prettier",
+      ],
+      parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: "script",
+      },
+      env: {
+        es6: true,
+        node: true,
+      },
+      rules: {
+        "eslint-comments/no-unused-disable": "error",
+      },
+    },
+  ],
+};
