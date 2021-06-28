b83f32ed3f9fc890094bdb7cd7c6e25e7f267f6a
Susisu <susisu2413@yahoo.co.jp>
2021-06-28 17:56:03 +0900

    chore(style): loosen lint rules for test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,14 @@ module.exports = {
       env: {
         "jest/globals": true,
       },
+      rules: {
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
+      },
     },
     // script files
     {
