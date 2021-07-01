bf35ed4de9095af5a1942650de9a6fc3fa1ef3ab
Susisu <susisu2413@yahoo.co.jp>
2021-07-02 01:24:41 +0900

    refactor: reorder declarations

--- src/core/impl.ts
+++ src/core/impl.ts
@@ -19,6 +19,44 @@ const uniqueId = (() => {
   };
 })();
 
+type Getter<A, B> = (a: A) => B;
+type Setter<A, B> = (a: A, b: B) => A;
+
+type Parent<T> = Readonly<{
+  attach: () => void;
+  detach: () => void;
+  setDefaultValue: (defaultValue: T) => void;
+  setValue: (value: T) => void;
+  setIsTouched: (isTouched: boolean) => void;
+  setIsDirty: (isDirty: boolean) => void;
+  setErrors: (errors: FieldErrors) => void;
+  setIsPending: (isPending: boolean) => void;
+}>;
+
+type Child<T> = Readonly<{
+  setDefaultValue: (defaultValue: T) => void;
+  setValue: (value: T) => void;
+  reset: () => void;
+  validate: () => void;
+  validateOnce: (value: T, signal: AbortSignal) => Promise<FieldErrors>;
+}>;
+
+type PendingValidation = Readonly<{ requestId: string; controller: AbortController }>;
+
+type MergeErrorsParams = Readonly<{
+  childrenErrors: FieldErrors;
+  validationErrors: FieldErrors;
+  customErrors: FieldErrors;
+}>;
+
+function mergeErrors(params: MergeErrorsParams): FieldErrors {
+  return {
+    ...params.childrenErrors,
+    ...params.validationErrors,
+    ...params.customErrors,
+  };
+}
+
 type FieldImplParams<T> = Readonly<{
   path: string;
   parent?: Parent<T> | undefined;
@@ -674,41 +712,3 @@ export class FieldImpl<T> implements FieldNode<T> {
     return Object.fromEntries(entries);
   }
 }
-
-type MergeErrorsParams = Readonly<{
-  childrenErrors: FieldErrors;
-  validationErrors: FieldErrors;
-  customErrors: FieldErrors;
-}>;
-
-function mergeErrors(params: MergeErrorsParams): FieldErrors {
-  return {
-    ...params.childrenErrors,
-    ...params.validationErrors,
-    ...params.customErrors,
-  };
-}
-
-type PendingValidation = Readonly<{ requestId: string; controller: AbortController }>;
-
-type Parent<T> = Readonly<{
-  attach: () => void;
-  detach: () => void;
-  setDefaultValue: (defaultValue: T) => void;
-  setValue: (value: T) => void;
-  setIsTouched: (isTouched: boolean) => void;
-  setIsDirty: (isDirty: boolean) => void;
-  setErrors: (errors: FieldErrors) => void;
-  setIsPending: (isPending: boolean) => void;
-}>;
-
-type Child<T> = Readonly<{
-  setDefaultValue: (defaultValue: T) => void;
-  setValue: (value: T) => void;
-  reset: () => void;
-  validate: () => void;
-  validateOnce: (value: T, signal: AbortSignal) => Promise<FieldErrors>;
-}>;
-
-type Getter<A, B> = (a: A) => B;
-type Setter<A, B> = (a: A, b: B) => A;
