0ebc7fde308a92f92b43c779a14e03f54030447a
Susisu <susisu2413@yahoo.co.jp>
2021-07-03 18:43:54 +0900

    test: FieldArrayImpl#getSnapshot

--- src/core/impl/fieldArray.spec.ts
+++ src/core/impl/fieldArray.spec.ts
@@ -25,4 +25,37 @@ describe("FieldArrayImpl", () => {
       expect(field2.id).not.toBe(field1.id);
     });
   });
+
+  describe("#getSnapshot", () => {
+    it("gets the latest snapshot of the field's state", () => {
+      const fieldArray = new FieldArrayImpl({
+        path: "$root",
+        defaultValue: [0],
+        value: [42],
+      });
+      // Initially the field array's state is initialized as follows:
+      // - 'defalutValue' and 'value' are set by the parameters
+      // - 'isTouched', 'isDirty', 'isPending' are set to false
+      // - 'errors' is set by the children
+      expect(fieldArray.getSnapshot()).toEqual({
+        defaultValue: [0],
+        value: [42],
+        isTouched: false,
+        isDirty: false,
+        errors: { 0: false },
+        isPending: false,
+      });
+
+      // After the state updated, 'getSnapshot' immediately returns the latest state.
+      fieldArray.setValue([1, 2]);
+      expect(fieldArray.getSnapshot()).toEqual({
+        defaultValue: [0],
+        value: [1, 2],
+        isTouched: false,
+        isDirty: false,
+        errors: { 0: false, 1: false },
+        isPending: false,
+      });
+    });
+  });
 });
