29861d366a260d68fef5774280c0e4480da880de
Susisu <susisu2413@yahoo.co.jp>
2021-07-04 16:53:21 +0900

    feat: wip implement Form

---
+++ src/core/impl/form.ts
@@ -0,0 +1,46 @@
+import {
+  Disposable,
+  FieldNode,
+  Form,
+  FormState,
+  FormStateSubscriber,
+  FormSubmitOptions,
+} from "../form";
+import { FieldNodeImpl } from "./fieldNode";
+
+export type FormImplParams<T> = Readonly<{
+  defaultValue: T;
+  value?: T;
+}>;
+
+export class FormImpl<T> implements Form<T> {
+  readonly root: FieldNode<T>;
+
+  constructor(params: FormImplParams<T>) {
+    this.root = new FieldNodeImpl({
+      path: "$root",
+      defaultValue: params.defaultValue,
+      value: params.value !== undefined ? params.value : params.defaultValue,
+    });
+  }
+
+  getState(): FormState {
+    throw new Error("not implemented");
+  }
+
+  subscribe(_subscriber: FormStateSubscriber): Disposable {
+    throw new Error("not implemented");
+  }
+
+  unsubscribe(_subscriber: FormStateSubscriber): void {
+    throw new Error("not implemented");
+  }
+
+  submit(_options?: FormSubmitOptions): Promise<void> {
+    throw new Error("not implemented");
+  }
+
+  reset(_value?: T): void {
+    throw new Error("not implemented");
+  }
+}
