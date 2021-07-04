a813393897b0f0be385c328e0a73503381d92ca5
Susisu <susisu2413@yahoo.co.jp>
2021-07-04 18:47:19 +0900

    feat: implement other FormImpl methods

--- src/core/impl/form.spec.ts
+++ src/core/impl/form.spec.ts
@@ -1,3 +1,5 @@
+import { waitForMicrotasks } from "../../__tests__/utils";
+import { FormSubmitRequest, ValidationRequest } from "../form";
 import { FormImpl } from "./form";
 
 describe("FormImpl", () => {
@@ -34,7 +36,508 @@ describe("FormImpl", () => {
     });
   });
 
+  describe("#getState", () => {
+    it("gets the latest snapshot of the form state", async () => {
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        handler: async () => {},
+      });
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+
+      await form.submit();
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: true,
+      });
+    });
+  });
+
+  describe("#subscribe", () => {
+    it("attaches a function that subscribes the form state", async () => {
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        handler: async () => {},
+      });
+      const subscriber = jest.fn(() => {});
+      const unsubscribe = form.subscribe(subscriber);
+
+      await waitForMicrotasks();
+      expect(subscriber).toHaveBeenCalledTimes(0);
+
+      await form.submit();
+      expect(subscriber).toHaveBeenCalledTimes(2);
+      expect(subscriber).toHaveBeenNthCalledWith(1, {
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+      expect(subscriber).toHaveBeenNthCalledWith(2, {
+        isSubmitting: false,
+        isSubmitted: true,
+      });
+
+      // can unsubscribe
+      unsubscribe();
+      form.reset();
+
+      expect(subscriber).toHaveBeenCalledTimes(2);
+      await waitForMicrotasks();
+      expect(subscriber).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  describe("#unsubscribe", () => {
+    it("detaches a function that subscribes the form state", async () => {
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        handler: async () => {},
+      });
+      const subscriber = jest.fn(() => {});
+      form.subscribe(subscriber);
+
+      await waitForMicrotasks();
+      expect(subscriber).toHaveBeenCalledTimes(0);
+
+      await form.submit();
+      expect(subscriber).toHaveBeenCalledTimes(2);
+      expect(subscriber).toHaveBeenNthCalledWith(1, {
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+      expect(subscriber).toHaveBeenNthCalledWith(2, {
+        isSubmitting: false,
+        isSubmitted: true,
+      });
+
+      form.unsubscribe(subscriber);
+      form.reset();
+
+      expect(subscriber).toHaveBeenCalledTimes(2);
+      await waitForMicrotasks();
+      expect(subscriber).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  describe("#submit", () => {
+    it("validates and submits the form using the handler", async () => {
+      const resolves: Array<() => void> = [];
+      const handler = jest.fn(
+        (_: FormSubmitRequest<{ x: number; y: number }>) =>
+          new Promise<void>(resolve => {
+            resolves.push(resolve);
+          })
+      );
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        value: { x: 42, y: 43 },
+        handler,
+      });
+      const validator = jest.fn((_: ValidationRequest<{ x: number; y: number }>) => {});
+      form.root.addValidator("foo", validator);
+      expect(validator).toHaveBeenCalledTimes(1);
+      const vRequest1 = validator.mock.calls[0][0];
+      expect(vRequest1).toEqual(
+        expect.objectContaining({
+          onetime: false,
+          value: { x: 42, y: 43 },
+        })
+      );
+      vRequest1.resolve(false);
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+
+      const subscriber = jest.fn(() => {});
+      form.subscribe(subscriber);
+
+      const promise = form.submit();
+
+      // validation phase
+      expect(validator).toHaveBeenCalledTimes(2);
+      const vRequest2 = validator.mock.calls[1][0];
+      expect(vRequest2).toEqual(
+        expect.objectContaining({
+          onetime: true,
+          value: { x: 42, y: 43 },
+        })
+      );
+      expect(handler).toHaveBeenCalledTimes(0);
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      expect(subscriber).toHaveBeenCalledTimes(0);
+      await waitForMicrotasks();
+      expect(subscriber).toHaveBeenCalledTimes(1);
+      expect(subscriber).toHaveBeenLastCalledWith({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      // submit phase
+      vRequest2.resolve(false);
+      expect(handler).toHaveBeenCalledTimes(0);
+      await waitForMicrotasks();
+      expect(handler).toHaveBeenCalledTimes(1);
+      const sRequest = handler.mock.calls[0][0];
+      expect(sRequest).toEqual({
+        id: expect.stringMatching(/^FormSubmitRequest\//),
+        value: { x: 42, y: 43 },
+        signal: expect.any(window.AbortSignal),
+      });
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      resolves[0]();
+      await promise;
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: true,
+      });
+      expect(subscriber).toHaveBeenCalledTimes(2);
+      expect(subscriber).toHaveBeenLastCalledWith({
+        isSubmitting: false,
+        isSubmitted: true,
+      });
+    });
+
+    it("fails if there are validation errors", async () => {
+      const handler = jest.fn(async () => {});
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        value: { x: 42, y: 43 },
+        handler,
+      });
+      const validator = jest.fn((_: ValidationRequest<{ x: number; y: number }>) => {});
+      form.root.addValidator("foo", validator);
+      expect(validator).toHaveBeenCalledTimes(1);
+      const vRequest1 = validator.mock.calls[0][0];
+      expect(vRequest1).toEqual(
+        expect.objectContaining({
+          onetime: false,
+          value: { x: 42, y: 43 },
+        })
+      );
+      vRequest1.resolve(true);
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+
+      const promise = form.submit();
+
+      // validation phase
+      expect(validator).toHaveBeenCalledTimes(2);
+      const vRequest2 = validator.mock.calls[1][0];
+      expect(vRequest2).toEqual(
+        expect.objectContaining({
+          onetime: true,
+          value: { x: 42, y: 43 },
+        })
+      );
+      expect(handler).toHaveBeenCalledTimes(0);
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      // submit phase
+      vRequest2.resolve(true);
+      await expect(promise).rejects.toThrowError('Invalid: {"foo":true}');
+      expect(handler).toHaveBeenCalledTimes(0);
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+    });
+
+    it("fails when the handler returns error", async () => {
+      const rejects: Array<(err: unknown) => void> = [];
+      const handler = jest.fn(
+        (_: FormSubmitRequest<{ x: number; y: number }>) =>
+          new Promise<void>((_, reject) => {
+            rejects.push(reject);
+          })
+      );
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        value: { x: 42, y: 43 },
+        handler,
+      });
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+
+      const promise = form.submit();
+      expect(handler).toHaveBeenCalledTimes(0);
+      await waitForMicrotasks();
+      expect(handler).toHaveBeenCalledTimes(1);
+      const sRequest = handler.mock.calls[0][0];
+      expect(sRequest).toEqual(expect.objectContaining({ value: { x: 42, y: 43 } }));
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      rejects[0](new Error("test error"));
+      await expect(promise).rejects.toThrowError("test error");
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+    });
+
+    it("is aborted if the signal has already been aborted", async () => {
+      const handler = jest.fn(async () => {});
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        value: { x: 42, y: 43 },
+        handler,
+      });
+      const validator = jest.fn((_: ValidationRequest<{ x: number; y: number }>) => {});
+      form.root.addValidator("foo", validator);
+      expect(validator).toHaveBeenCalledTimes(1);
+      const vRequest = validator.mock.calls[0][0];
+      expect(vRequest).toEqual(
+        expect.objectContaining({
+          onetime: false,
+          value: { x: 42, y: 43 },
+        })
+      );
+      vRequest.resolve(false);
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+
+      const controller = new window.AbortController();
+      controller.abort();
+      const promise = form.submit({ signal: controller.signal });
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+
+      await expect(promise).rejects.toThrowError("Aborted");
+      expect(validator).toHaveBeenCalledTimes(1);
+      expect(handler).toHaveBeenCalledTimes(0);
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+    });
+
+    it("is aborted when the signal is aborted", async () => {
+      const handler = jest.fn(
+        (_: FormSubmitRequest<{ x: number; y: number }>) => new Promise<void>(() => {})
+      );
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        value: { x: 42, y: 43 },
+        handler,
+      });
+      const validator = jest.fn((_: ValidationRequest<{ x: number; y: number }>) => {});
+      form.root.addValidator("foo", validator);
+      expect(validator).toHaveBeenCalledTimes(1);
+      const vRequest1 = validator.mock.calls[0][0];
+      expect(vRequest1).toEqual(
+        expect.objectContaining({
+          onetime: false,
+          value: { x: 42, y: 43 },
+        })
+      );
+      vRequest1.resolve(false);
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+
+      const controller = new window.AbortController();
+      const promise = form.submit({ signal: controller.signal });
+
+      // validation phase
+      expect(validator).toHaveBeenCalledTimes(2);
+      const vRequest2 = validator.mock.calls[1][0];
+      expect(vRequest2).toEqual(
+        expect.objectContaining({
+          onetime: true,
+          value: { x: 42, y: 43 },
+        })
+      );
+      expect(handler).toHaveBeenCalledTimes(0);
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      // submit phase
+      vRequest2.resolve(false);
+      expect(handler).toHaveBeenCalledTimes(0);
+      await waitForMicrotasks();
+      expect(handler).toHaveBeenCalledTimes(1);
+      const sRequest = handler.mock.calls[0][0];
+      expect(sRequest).toEqual(expect.objectContaining({ value: { x: 42, y: 43 } }));
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      const onAbort = jest.fn(() => {});
+      sRequest.signal.addEventListener("abort", onAbort);
+
+      controller.abort();
+      expect(onAbort).toHaveBeenCalled();
+
+      await expect(promise).rejects.toThrowError("Aborted");
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+    });
+
+    it("set isSubmitting = false after all the pending request are done", async () => {
+      const resolves: Array<() => void> = [];
+      const handler = jest.fn(
+        (_: FormSubmitRequest<{ x: number; y: number }>) =>
+          new Promise<void>(resolve => {
+            resolves.push(resolve);
+          })
+      );
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        value: { x: 42, y: 43 },
+        handler,
+      });
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+
+      const promise1 = form.submit();
+      expect(handler).toHaveBeenCalledTimes(0);
+      await waitForMicrotasks();
+      expect(handler).toHaveBeenCalledTimes(1);
+      const sRequest1 = handler.mock.calls[0][0];
+      expect(sRequest1).toEqual(expect.objectContaining({ value: { x: 42, y: 43 } }));
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      const promise2 = form.submit();
+      expect(handler).toHaveBeenCalledTimes(1);
+      await waitForMicrotasks();
+      expect(handler).toHaveBeenCalledTimes(2);
+      const sRequest2 = handler.mock.calls[1][0];
+      expect(sRequest2).toEqual(expect.objectContaining({ value: { x: 42, y: 43 } }));
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      resolves[0]();
+      await promise1;
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: true,
+      });
+
+      resolves[1]();
+      await promise2;
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: true,
+      });
+    });
+
+    it("does not set isSubmitted = true if the form is reset after submitted", async () => {
+      const resolves: Array<() => void> = [];
+      const handler = jest.fn(
+        (_: FormSubmitRequest<{ x: number; y: number }>) =>
+          new Promise<void>(resolve => {
+            resolves.push(resolve);
+          })
+      );
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        value: { x: 42, y: 43 },
+        handler,
+      });
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+
+      const promise1 = form.submit();
+      expect(handler).toHaveBeenCalledTimes(0);
+      await waitForMicrotasks();
+      expect(handler).toHaveBeenCalledTimes(1);
+      const sRequest1 = handler.mock.calls[0][0];
+      expect(sRequest1).toEqual(expect.objectContaining({ value: { x: 42, y: 43 } }));
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      form.reset();
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      const promise2 = form.submit();
+      expect(handler).toHaveBeenCalledTimes(1);
+      await waitForMicrotasks();
+      expect(handler).toHaveBeenCalledTimes(2);
+      const sRequest2 = handler.mock.calls[1][0];
+      expect(sRequest2).toEqual(expect.objectContaining({ value: { x: 0, y: 1 } }));
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      resolves[0]();
+      await promise1;
+      expect(form.getState()).toEqual({
+        isSubmitting: true,
+        isSubmitted: false,
+      });
+
+      resolves[1]();
+      await promise2;
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: true,
+      });
+    });
+  });
+
   describe("#reset", () => {
+    it("resets the form state", async () => {
+      const form = new FormImpl({
+        defaultValue: { x: 0, y: 1 },
+        handler: async () => {},
+      });
+      await form.submit();
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: true,
+      });
+
+      form.reset();
+      expect(form.getState()).toEqual({
+        isSubmitting: false,
+        isSubmitted: false,
+      });
+    });
+
     it("resets the field", () => {
       const form = new FormImpl({
         defaultValue: { x: 0, y: 1 },
--- src/core/impl/form.ts
+++ src/core/impl/form.ts
@@ -4,17 +4,32 @@ import {
   Form,
   FormState,
   FormStateSubscriber,
+  FormSubmitHandler,
   FormSubmitOptions,
+  isValid,
 } from "../form";
 import { FieldNodeImpl } from "./fieldNode";
+import { uniqueId } from "./shared";
 
 export type FormImplParams<T> = Readonly<{
   defaultValue: T;
   value?: T;
+  handler: FormSubmitHandler<T>;
 }>;
 
 export class FormImpl<T> implements Form<T> {
   readonly root: FieldNode<T>;
+
+  private handler: FormSubmitHandler<T>;
+
+  private sessionId: string;
+
+  private pendingRequestIds: Set<string>;
+  private isSubmitted: boolean;
+
+  private state: FormState;
+  private subscribers: Set<FormStateSubscriber>;
+  private isDispatchQueued: boolean;
 
   constructor(params: FormImplParams<T>) {
     this.root = new FieldNodeImpl({
@@ -22,22 +37,120 @@ export class FormImpl<T> implements Form<T> {
       defaultValue: params.defaultValue,
       value: params.value !== undefined ? params.value : params.defaultValue,
     });
+
+    this.handler = params.handler;
+
+    this.sessionId = `FormSession/${uniqueId()}`;
+
+    this.pendingRequestIds = new Set();
+    this.isSubmitted = false;
+
+    this.state = {
+      isSubmitting: this.calcStateIsSubmitting(),
+      isSubmitted: this.calcStateIsSubmitted(),
+    };
+    this.subscribers = new Set();
+    this.isDispatchQueued = false;
+  }
+
+  // depends on: pendingRequestIds
+  private calcStateIsSubmitting(): boolean {
+    return this.pendingRequestIds.size > 0;
+  }
+
+  // depends on: isSubmitted
+  private calcStateIsSubmitted(): boolean {
+    return this.isSubmitted;
   }
 
   getState(): FormState {
-    throw new Error("not implemented");
+    return this.state;
   }
 
-  subscribe(_subscriber: FormStateSubscriber): Disposable {
-    throw new Error("not implemented");
+  subscribe(subscriber: FormStateSubscriber): Disposable {
+    this.subscribers.add(subscriber);
+    return () => {
+      this.unsubscribe(subscriber);
+    };
   }
 
-  unsubscribe(_subscriber: FormStateSubscriber): void {
-    throw new Error("not implemented");
+  unsubscribe(subscriber: FormStateSubscriber): void {
+    this.subscribers.delete(subscriber);
   }
 
-  submit(_options?: FormSubmitOptions): Promise<void> {
-    throw new Error("not implemented");
+  private queueDispatch(): void {
+    if (this.isDispatchQueued) {
+      return;
+    }
+    this.isDispatchQueued = true;
+
+    window.queueMicrotask(() => {
+      this.isDispatchQueued = false;
+
+      const state = this.state;
+      for (const subscriber of [...this.subscribers]) {
+        try {
+          subscriber(state);
+        } catch (err: unknown) {
+          // eslint-disable-next-line no-console
+          console.error(err);
+        }
+      }
+    });
+  }
+
+  private updateStateIsSubmitting(): void {
+    const isSubmitting = this.calcStateIsSubmitting();
+    if (this.state.isSubmitting === isSubmitting) {
+      return;
+    }
+    this.state = { ...this.state, isSubmitting };
+    this.queueDispatch();
+  }
+
+  private updateStateIsSubmitted(): void {
+    const isSubmitted = this.calcStateIsSubmitted();
+    if (this.state.isSubmitted === isSubmitted) {
+      return;
+    }
+    this.state = { ...this.state, isSubmitted };
+    this.queueDispatch();
+  }
+
+  async submit(options?: FormSubmitOptions): Promise<void> {
+    const signal = options?.signal;
+    const controller = new window.AbortController();
+    if (signal) {
+      if (signal.aborted) {
+        throw new Error("Aborted");
+      }
+      signal.addEventListener("abort", () => {
+        controller.abort();
+      });
+    }
+    const sessionId = this.sessionId;
+    const id = `FormSubmitRequest/${uniqueId()}`;
+    this.pendingRequestIds.add(id);
+    this.updateStateIsSubmitting();
+    try {
+      const { value, errors } = await this.root.validateOnce({ signal: controller.signal });
+      if (!isValid(errors)) {
+        throw new Error(`Invalid: ${JSON.stringify(errors)}`); // more useful error?
+      }
+      await new Promise((resolve, reject) => {
+        controller.signal.addEventListener("abort", () => {
+          reject(new Error("Aborted"));
+        });
+        this.handler({ id, value, signal: controller.signal }).then(resolve, reject);
+      });
+      if (this.sessionId === sessionId) {
+        this.isSubmitted = true;
+        this.updateStateIsSubmitted();
+      }
+    } finally {
+      this.pendingRequestIds.delete(id);
+      this.updateStateIsSubmitting();
+    }
   }
 
   reset(value?: T): void {
@@ -45,5 +158,10 @@ export class FormImpl<T> implements Form<T> {
       this.root.setDefaultValue(value);
     }
     this.root.reset();
+
+    this.isSubmitted = false;
+    this.updateStateIsSubmitted();
+
+    this.sessionId = `FormSession/${uniqueId()}`;
   }
 }
