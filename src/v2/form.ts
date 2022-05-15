ed24fd4da7e99d753a87a590579e70bd0c2908fc
Susisu <susisu2413@yahoo.co.jp>
2022-05-15 18:50:34 +0900

    feat: add Form type

--- src/v2/form.ts
+++ src/v2/form.ts
@@ -1,1 +1,43 @@
-export {};
+import { Field } from "./field";
+
+export type Form<T> = Readonly<{
+  id: string;
+  root: Field<T>; // TODO: FieldNode<T>
+
+  getState: () => FormState;
+  subscribeState: (subscriber: FormStateSubscriber) => void;
+  unsubscribeState: (subscriber: FormStateSubscriber) => void;
+
+  submit: <R>(
+    action: FormSubmitAction<T, R>,
+    options?: FormSubmitOptions
+  ) => Promise<FormSubmitResult<R>>;
+
+  reset: (value?: T) => void;
+}>;
+
+export type FormState = Readonly<{
+  isSubmitting: boolean;
+  submitCount: number;
+}>;
+
+export type FormStateSubscriber = (stat: FormState) => void;
+
+export type FormSubmitAction<T, R> = (req: FormSubmitRequest<T>) => Promise<R>;
+
+export type FormSubmitRequest<T> = Readonly<{
+  id: string;
+  value: T;
+  signal: AbortSignal;
+}>;
+
+export type FormSubmitOptions = Readonly<{
+  skipValidation?: boolean | undefined;
+  signal?: AbortSignal | undefined;
+}>;
+
+export type FormSubmitResult<R> =
+  | Readonly<{ type: "success"; data: R }>
+  | Readonly<{ type: "canceled"; reason: FormSubmitCancellationReason }>;
+
+export type FormSubmitCancellationReason = "validationError" | "aborted";
